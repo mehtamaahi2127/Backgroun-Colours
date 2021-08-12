80a3d207f51a9319d34db749016182b02d7cd4b6
mehtamaahi2127 <88487618+mehtamaahi2127@users.noreply.github.com>
2021-08-12 16:01:34 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+  background(30);
+if(keyDown("left")){
+  background("blue")
+}
+if(keyDown("right")){
+  background("red")
+}
+if(keyDown("up")){
+  background("yellow")
+}
+if(keyDown("down")){
+  background("green")
+}
+
+
+
+}
+
+
+
+
